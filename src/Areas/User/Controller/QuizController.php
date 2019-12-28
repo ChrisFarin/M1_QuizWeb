fc596803b6a4587af52f078847a4a6582bc16a81
Chris <farinchris@gmail.com>
2019-12-28 13:57:38 +0100

    Modification agencement des onglets

--- src/Areas/User/Controller/QuizController.php
+++ src/Areas/User/Controller/QuizController.php
@@ -2,18 +2,46 @@
 
 namespace App\Areas\User\Controller;
 
+use App\Entity\User;
+use App\Entity\Quiz;
+use App\Areas\User\Form\CreateQuizFormType;
+use App\Areas\User\Security\Authenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Security;
 
 class QuizController extends AbstractController
 {
     /**
-     * @Route("/create", name="quiz")
+     * @Route("/create", name="app_create_quiz")
      */
-    public function create()
+    public function create(Request $request, Security $security)
     {
+        $quiz = new Quiz();
+        $form = $this->createForm(CreateQuizFormType::class, $quiz);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $quiz->setName($form->get('Name')->getData());
+            $quiz->setIsVisible($form->get('isVisible')->getData());
+            $quiz->setDate(new \DateTime('now'));
+            $usr = $security->getUser();
+            $quiz->setAuthor($usr);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($quiz);
+            $entityManager->flush();
+
+            // On redirige vers la création de questions
+            //return $this->redirectToRoute(route:'/create/question');
+
+        }
+
         return $this->render('Areas/User/quiz/create.html.twig', [
-            'controller_name' => 'QuizController',
+            'CreateQuizForm' => $form->createView()
         ]);
     }
 
