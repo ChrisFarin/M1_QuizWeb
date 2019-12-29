f122341fbbf02742d2c9b028cb4ea188be4b667a
Chris <farinchris@gmail.com>
2019-12-29 14:22:47 +0100

    Page de création d'un quiz

--- src/Areas/User/Form/CreateQuizFormType.php
+++ src/Areas/User/Form/CreateQuizFormType.php
@@ -7,6 +7,7 @@ use App\Entity\Quiz;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -18,10 +19,13 @@ class CreateQuizFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('Name')
+            ->add('Name', TextType::class, [
+                'label' => 'Nom du quiz',
+            ])
             ->add('isVisible', CheckboxType::class, [
                 'mapped' => true,
                 'required' => false,
+                'label' => 'Visible'
 
             ])
             ->setMethod('GET')
